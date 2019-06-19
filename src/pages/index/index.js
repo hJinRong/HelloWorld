fa1dada04a1dccee6a352816492dbe528b6c3155
Rong <rong161514@gmail.com>
2019-06-19 15:56:14 +0800

    修改index界面，一次引入index.css

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,27 +1,61 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { AtTabs, AtTabsPane, AtAvatar, AtDrawer, AtIcon } from 'taro-ui'
 import './index.scss'
 
 export default class Index extends Component {
-
-  config = {
-    navigationBarTitleText: '首页'
+  constructor(props) {
+    super(props)
+    this.state = {
+      show: false,
+      current: 0,
+      nickName: '',
+      avatarUrl: '',
+      province: '',
+      city: ''
+    }
   }
 
-  componentWillMount () { }
+  config = {
+    navigationBarTitleText: '今日天气'
+  }
 
-  componentDidMount () { }
+  componentWillMount() { }
 
-  componentWillUnmount () { }
+  componentDidMount() {
+    Taro.getUserInfo({
+      success: function (res) {
+        let userInfo = res.userInfo
+        let nickName = userInfo.nickName
+        let avatarUrl = userInfo.avatarUrl
+        let province = userInfo.province
+        let city = userInfo.city
+        this.setState({
+          nickName: nickName,
+          avatarUrl: avatarUrl,
+          province: province,
+          city: city
+        })
+      }
+    })
+  }
 
-  componentDidShow () { }
+  componentWillUnmount() { }
 
-  componentDidHide () { }
+  componentDidShow() { }
 
-  render () {
+  componentDidHide() { }
+
+  render() {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <AtDrawer show={this.state.show} mask width='230px' right='true'>
+          <View><AtIcon size='20' value='bell'></AtIcon>做个备忘</View>
+          <View><AtIcon size='20' value='star-2'></AtIcon>Star for me</View>
+          <View><AtIcon size='20' value='help'></AtIcon>About us</View>
+          <View><AtIcon size='20' value='allow-left'></AtIcon>Exit</View>
+        </AtDrawer>
+        
       </View>
     )
   }
